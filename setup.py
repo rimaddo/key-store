4cb001b5c04a8a4347133c1158af623ec6c2686d
rachel madden <rachel.isabella.madden@gmail.com>
2020-11-01 10:58:51 +0000

    Added setup  py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='key_store',
-    version='0.1.0',
+    version='0.1.1',
     description='Helper functions for getting objects easily from a list with multiple filter keys',
     url='https://github.com/rimaddo/key-store',
     maintainer='Rachel',
