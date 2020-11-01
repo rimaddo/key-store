221cd43e7eeead26a13b21b4feca99f8fb320583
rachel madden <rachel.isabella.madden@gmail.com>
2020-11-01 10:45:18 +0000

    Added set-uppy

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='key_store',
+    version='0.1.0',
+    description='Helper functions for getting objects easily from a list with multiple filter keys',
+    url='https://github.com/rimaddo/key-store',
+    maintainer='Rachel',
+    maintainer_email='rachel.isabella.madden@gmail.com',
+    license='MIT',
+    packages=['key_store'],
+    python_requires='>3.7.0',
+)
